4cdf36a8ceedbdc987e6cb233d72f7c72beb4aa6
Jiaxin Pu <jiaxin.pu@thoughtworks.com>
2024-05-21 16:53:52 +0800

    refactor code

--- src/main/java/org/example/BowlingGame.java
+++ src/main/java/org/example/BowlingGame.java
@@ -1,48 +1,64 @@
 package org.example;
 
 public class BowlingGame {
+    private static final int MAXIMUM_PINS = 10;
+    private static final int FRAME_COUNT = 10;
+
     private int[] rolls = new int[21];
-    private int currentRoll = 0;
+    private int currentRollIndex = 0;
 
     public void roll(int pins) {
-        rolls[currentRoll++] = pins;
+        if (pins < 0 || pins > MAXIMUM_PINS) {
+            throw new IllegalArgumentException("Invalid number of pins knocked down: " + pins);
+        }
+        rolls[currentRollIndex++] = pins;
     }
 
     public int score() {
-        int score = 0;
-        int frameIndex = 0;
-        for (int frame = 0; frame < 10; frame++) {
-            if (isStrike(frameIndex)) {
-                score += 10 + strikeBonus(frameIndex);
-                frameIndex++;
-            } else if (isSpare(frameIndex)) {
-                score += 10 + spareBonus(frameIndex);
-                frameIndex += 2;
-            } else {
-                score += sumOfBallsInFrame(frameIndex);
-                frameIndex += 2;
-            }
+        int totalScore = 0;
+        int currentFrameIndex = 0;
+        for (int frame = 0; frame < FRAME_COUNT; frame++) {
+            totalScore += calculateFrameScore(currentFrameIndex);
+            currentFrameIndex += getFrameRollCount(currentFrameIndex);
         }
-        return score;
+        return totalScore;
+    }
+
+    private int calculateFrameScore(int frameIndex) {
+        if (isStrike(frameIndex)) {
+            return MAXIMUM_PINS + calculateBonusScore(frameIndex);
+        } else if (isSpare(frameIndex)) {
+            return MAXIMUM_PINS + calculateBonusScore(frameIndex);
+        } else {
+            return sumOfBallsInFrame(frameIndex);
+        }
+    }
+
+    private int calculateBonusScore(int frameIndex) {
+        if (isStrike(frameIndex)) {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        } else {
+            return rolls[frameIndex + 2];
+        }
     }
 
     private boolean isStrike(int frameIndex) {
-        return rolls[frameIndex] == 10;
+        return rolls[frameIndex] == MAXIMUM_PINS;
     }
 
     private boolean isSpare(int frameIndex) {
-        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
-    }
-
-    private int strikeBonus(int frameIndex) {
-        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
-    }
-
-    private int spareBonus(int frameIndex) {
-        return rolls[frameIndex + 2];
+        return rolls[frameIndex] + rolls[frameIndex + 1] == MAXIMUM_PINS;
     }
 
     private int sumOfBallsInFrame(int frameIndex) {
         return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+
+    private int getFrameRollCount(int frameIndex) {
+        if (isStrike(frameIndex)) {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 }
